b23cea72fe672d30f9ca8ba9a2000096715f1715
Atanas Atanasov <scriptex.bg@gmail.com>
2020-09-01 10:33:45 +0300

    Upgrade packages

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.Panel = exports.Accordion = void 0;
 var accordion_1 = require("./accordion");
-exports.Accordion = accordion_1.default;
+Object.defineProperty(exports, "Accordion", { enumerable: true, get: function () { return accordion_1.default; } });
 var panel_1 = require("./panel");
-exports.Panel = panel_1.default;
+Object.defineProperty(exports, "Panel", { enumerable: true, get: function () { return panel_1.default; } });
 //# sourceMappingURL=index.js.map
