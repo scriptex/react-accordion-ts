d0d6761b8b4e6f7509092d200e5f4a2532766756
Atanas Atanasov <scriptex.bg@gmail.com>
2020-10-06 17:12:17 +0300

    Remove /dist from git

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Panel = exports.Accordion = void 0;
-var accordion_1 = require("./accordion");
-Object.defineProperty(exports, "Accordion", { enumerable: true, get: function () { return accordion_1.default; } });
-var panel_1 = require("./panel");
-Object.defineProperty(exports, "Panel", { enumerable: true, get: function () { return panel_1.default; } });
-//# sourceMappingURL=index.js.map
